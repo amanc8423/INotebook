4d9a3bc7491fe6ce7ceac07b528e5f95b9a5f2a5
amanc8423 <amanc8423@gmail.com>
2023-04-24 19:12:48 +0530

    added data using post request

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 
 const mongoose = require('mongoose');
 mongoose.connect("mongodb://127.0.0.1:27017/notebook");
+app.use(express.json());
 
 app.use('/api/auth',require('./routes/auth'));
 
@@ -10,5 +12,11 @@ app.use('/api/notes',require('./routes/notes'));
 app.listen(5000,()=>{console.log("server on")});
 
 
+
+
+
+/*
+The express.json() function is a built-in middleware function in Express. It parses incoming requests with JSON payloads and is based on body-parser. 
+*/
 
 
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const UserSchema= new Schema({
     name:{
@@ -19,3 +20,5 @@ const UserSchema= new Schema({
         default:Date.now
     }
 })
+
+module.exports=mongoose.model('User',UserSchema);
